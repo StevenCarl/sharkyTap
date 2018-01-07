ba16d6dc5cb944035d5aa7dad8434663ba040351
CJIgnacio <carljhonsteven@gmail.com>
2018-01-07 16:31:47 +0800

    Deleted unused data

---
+++ app/src/main/java/com/example/cjignacio/tapsharky/Trivia.java
@@ -0,0 +1,14 @@
+package com.example.cjignacio.tapsharky;
+
+/**
+ * Created by CJIgnacio on 12/27/2017.
+ */
+
+public class Trivia {
+
+    public static final String [] trivias = {
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+            "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+            "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+    };
+}
