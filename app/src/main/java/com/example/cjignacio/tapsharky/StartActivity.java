7b8f1749a3d795f8f65d047a65c6d9267d63b43e
CJIgnacio <carljhonsteven@gmail.com>
2018-01-09 14:20:33 +0800

    Added Settings

--- app/src/main/java/com/example/cjignacio/tapsharky/StartActivity.java
+++ app/src/main/java/com/example/cjignacio/tapsharky/StartActivity.java
@@ -1,45 +1,62 @@
 package com.example.cjignacio.tapsharky;
 
+import android.app.Dialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class StartActivity extends AppCompatActivity {
 
     Button play;
     MediaPlayer mp;
+    Dialog myDialog;
+    TextView txtclose;
+    Button btnSave;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
-
+        myDialog = new Dialog(this);
         // For Background Music
         play = (Button) findViewById(R.id.button_play);
-        mp = MediaPlayer.create(StartActivity.this, R.raw.babyshark);
-        mp.start();
-        mp.setLooping(true);
+//        mp = MediaPlayer.create(StartActivity.this, R.raw.babyshark);
+//        mp.start();
+//        mp.setLooping(true);
         play.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (mp.isPlaying()) {
-                    mp.pause();
-                    play.setBackgroundResource(R.drawable.mute);
-                } else {
-                    ;
-                    mp.start();
-                    play.setBackgroundResource(R.drawable.music);
-                }
+                myDialog.setContentView(R.layout.activity_settings);
+                txtclose = (TextView) myDialog.findViewById(R.id.closetxt);
+                btnSave = (Button) myDialog.findViewById(R.id.saveBtn);
+                txtclose.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        myDialog.dismiss();
+                    }
+                });
+                myDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                myDialog.show();
+//                if (mp.isPlaying()) {
+//                    mp.pause();
+//                    play.setBackgroundResource(R.drawable.mute);
+//                } else {
+//                    ;
+//                    mp.start();
+//                    play.setBackgroundResource(R.drawable.music);
+//                }
             }
         });
 
     }
-
     // Double Backpress will exit the app
     boolean twice = false;
 
@@ -61,7 +78,7 @@ public class StartActivity extends AppCompatActivity {
     }
 
     public void startGame(View view) {
-        mp.stop();
+//        mp.stop();
         startActivity(new Intent(getApplicationContext(), MainActivity.class));
     }
 
